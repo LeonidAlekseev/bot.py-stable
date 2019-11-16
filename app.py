a61b8bb7f7cf57437092e7433b3fa5bcb661533d
LeonidAlekseev <55627946+LeonidAlekseev@users.noreply.github.com>
2019-11-16 19:20:45 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -314,7 +314,7 @@ def index():
                 text_ocr = get_ocr(path_to_download)
                 send_message(chat_id, text="Вот что у нас получилось:")
                 send_message(chat_id, text=text_ocr)
-                send_message(chat_id, text="1) Проверьте правильность распознования \n2) Скопируйте код \n3) Отправьте его нам для исполнения")
+                send_message(chat_id, text="1) Проверьте правильность распознования \n2) Скопируйте код \n3) Отправьте его нам для исполнения\n**Если вы что-то упустите, мы подскажем, где ошибка)")
             elif message != "":
                 result = cms(message)
                 send_message(chat_id, text=result)
