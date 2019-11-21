a0c4c4a72d34a893525415bbab973a485dd8b8dd
LeonidAlekseev <55627946+LeonidAlekseev@users.noreply.github.com>
2019-11-21 12:21:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -262,7 +262,7 @@ def cms(wel):
         exit=f.read()
         if 'Traceback' in exit:
             lines = open('help.txt').readlines()
-            open('helpfull.txt', 'w').writelines(lines[-20:-10]+lines[-1])
+            open('helpfull.txt', 'w').writelines(lines[-20:])
             open('help.txt', 'w').writelines(lines[5:-1])
             preexit=open('help.txt').read()
             nameerrexit=str(open('help.txt').readlines())
