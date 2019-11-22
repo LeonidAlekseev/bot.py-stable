e07b4207b9f865aa6d0fca39339276db98113fef
LeonidAlekseev <55627946+LeonidAlekseev@users.noreply.github.com>
2019-11-22 15:17:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+from tabulate import tabulate
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -25,11 +25,9 @@ from PIL import Image
 import pytesseract
 import io
 import urllib.request
-
-
+import signal
 app = Flask(__name__)
 sslify = SSLify(app)
-
 
 token = str(os.environ.get("TOKEN"))+"/"
 mongo_login=str(os.environ.get("MONGO_LOGIN"))
@@ -37,14 +35,13 @@ mongo_cluster=str(os.environ.get("MONGO_CLUSTER"))
 mongo_collection=str(os.environ.get("MONGO_COLLECTION"))
 URL = 'https://api.telegram.org/bot'+token
 pytesseract.pytesseract.tesseract_cmd = '/app/vendor/tesseract-ocr/bin/tesseract'
-
+now = datetime.datetime.now()
 
 def send_message(chat_id, text='Какой-то текст.'):
     url = URL + 'sendMessage'
     answer = {'chat_id': chat_id, 'text': text}
     r = requests.post(url, json=answer)
     return r.json()
-
 #Line search
 def w_line(text):
     c=text.count("line")
@@ -61,12 +58,11 @@ def w_line(text):
     line=line_n.replace(',','')
     return line
 #-Line search
-
 #Definitions
 def transtlate(errname):
-    if "ModuleNotFoundError" in errname:
-        sut="не импортирован вызываемый модуль"
-    if "BaseException" in errname:
+    if "BlockInfinityErrore" in errname:
+        sut="время обработки операции превысило допустимое значение (бесконечные циклы запрещены)"
+    elif "BaseException" in errname:
         sut="базовое исключение, от которого берут начало все остальные"
     elif "SystemExit" in errname:
         sut="исключение, порождаемое функцией sys.exit при выходе из программы"
@@ -168,16 +164,16 @@ def transtlate(errname):
         sut="исключение, связанное с переводом unicode"
     elif "Warning" in errname:
         sut="предупреждение"
+    elif "ModuleNotFoundError" in errname:
+        sut="не импортирован вызываемый модуль"
     else:
         sut="ошибка не опознана"
     return sut
 #-Definitions
-
 #MongoDB
 cluster=MongoClient("mongodb+srv://"+mongo_login+"@cluster0-umqxp.mongodb.net/test?retryWrites=true&w=majority")
 db = cluster[mongo_cluster]
 collection = db[mongo_collection]
-
 def find_string(user):
     answer = False
     post = {"user":user}
@@ -186,7 +182,6 @@ def find_string(user):
         if result != '':
             answer = True
     return answer
-
 def find_password(user):
     answer = False
     post = {"user":user}
@@ -197,100 +192,116 @@ def find_password(user):
         if check != "0":
             answer = True
     return answer
-
 def new_password(user,password):
     user_post = {"user":user}
     pass_post = {"$set":{"password":password}}
     results = collection.update_one(user_post,pass_post)
-
 def new_user(user):
     post = {"user": user,"password": "0"}
     collection.insert_one(post)
 #-MongoDB
-
 #Auth
 def key():
-    now = datetime.datetime.now()
     key = str(math.fabs(math.sin(now.minute+now.day)))[2:7]
     return key+'-LIL'
-
 def add_key(user):
     new_user(user)
-
 def add_pass(user,pas):
     new_password(user,pas)
-
 def check_key(user):
     if find_string(user):
         return 'yes'
     else:
         return 'no'
-
 def check_pass(user):
     if find_password(user):
         return 'yes'
     else:
         return 'no'
 #-Auth
-
 #Cms
 def cms(wel):
+    lineerrexit=""
     with open('help.txt', 'w') as f:
         with redirect_stdout(f):
-            if "while True" not in wel and "input(" not in wel:
+            if "input(" not in wel:
                 try:
-                    exec(wel)
+                    def long_function_call(inp):
+                        exec(inp)
+                    def signal_handler(signum, frame):
+                        raise Exception("BlockInfinityErrore")
+                    signal.signal(signal.SIGALRM, signal_handler)
+                    #how much seconds
+                    signal.alarm(3)
+                    long_function_call(wel)
                 except Exception:
                     print(traceback.format_exc())
             else:
-                if "while True" in wel:
-                    print("Бесконечный цикл невозможен")
                 if "input(" in wel:
                     print("Ввод невозможен")
     with open('help.txt', 'r') as f:
         exit=f.read()
         if 'Traceback' in exit:
             lines = open('help.txt').readlines()
-            open('help.txt', 'w').writelines(lines[3:-1])
+            open('helpfull.txt', 'w').writelines(lines[-20:-10])
+            open('helpfull.txt', 'a').writelines(lines[-2])
+            open('help.txt', 'w').writelines(lines[5:-1])
             preexit=open('help.txt').read()
             nameerrexit=str(open('help.txt').readlines())
-            lineerrexit=w_line(str(preexit)) #open('help.txt').readlines()[0].split(',')[1]
-            #lineerrexit=lineerrexit.replace("line", "линия")
+            lineerrexit="линия " + str(w_line(str(preexit)))
             sutexit=transtlate(nameerrexit)
-            exit='\u26A0 ОШИБКА \u26A0 \n'+preexit+'\n\uD83D\uDE3B РАСШИФРОВКА \uD83D\uDE3B \n  Суть ошибки: '+sutexit+'\n  Место ошибки: линия '+lineerrexit
+            if "BlockInfinityErrore" in nameerrexit:
+                preexit=open('helpfull.txt').read()
+                lineerrexit="неопределено"
+            exit='\u26A0 ОШИБКА \u26A0 \n'+preexit+'\n\uD83D\uDE3B ПОЯСНЕНИЕ \uD83D\uDE3B \n  Суть ошибки: '+sutexit+'\n  Место ошибки: '+lineerrexit
         else:
             exit=open('help.txt', 'r').read()
-    return exit
+            if exit == '' or exit == ' ':
+                exit='В консоль ничего не вывелось! Проверьте, пожалуйста, возможно где-то не хватает print() или return.'
+    return exit, lineerrexit
 #-Cms
-
 #OCR
 def get_ocr(url):
     #download file
     filename = url.split("/")[-1]
+    send_message(676318616, text="1")
     filetype = filename.split('.')
+    send_message(676318616, text="2")
     if filetype[-1] != "jpg" and filetype[-1] != "png":
         return 111
+    send_message(676318616, text="3")
     response = requests.get(url, stream=True)
+    send_message(676318616, text="4")
     with open(str(filename), 'wb') as out_file:
         shutil.copyfileobj(response.raw, out_file)
+    send_message(676318616, text="5")
     del response
+    send_message(676318616, text="6")
     #size of image
     img = Image.open(filename)
+    send_message(676318616, text="7")
     new_size = tuple(4*x for x in img.size)
+    send_message(676318616, text="8")
     img = img.resize(new_size, Image.ANTIALIAS)
+    send_message(676318616, text="9")
     img.save("4x"+filename)
+    send_message(676318616, text="10")
     #pytesseract
     text = pytesseract.image_to_string(Image.open("4x"+filename))
+    send_message(676318616, text="11")
     text = text.replace("\n\n","\n")
+    send_message(676318616, text="12")
     if text != '':
         return text
     else:
         return 222
+    send_message(676318616, text="1")
     #delete file
     os.remove(filename)
+    send_message(676318616, text="1")
     os.remove("4x"+filename)
+    send_message(676318616, text="1")
 #-OCR
-
 #Flask
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -348,28 +359,47 @@ def index():
                     send_message(chat_id, text=my_string)
                     send_message(chat_id, text="1) Проверьте правильность распознования \n2) Скопируйте код \n3) Отправьте его нам для исполнения\n**Если вы что-то упустите, мы подскажем, где ошибка!")
             elif message != "":
-                result = cms(message)
+                if now.hour+3 == 4 and 0<now.minute<5:
+                    result = "Бот перезагружен. Приятной работы!"
+                else:
+                    try:
+                        result = cms(message)[0]
+                        #----------------------restart----------------------
+                        #result = "Бот остановлен! Обратитесь к отцу бота! Lil Dojd - https://vk.com/misterlil"
+                        #---------------------------------------------------
+                    except BaseException:
+                        result = "Ошибка сиситемы. Код не может быть выполнен. Обратитесь к отцу бота! Lil Dojd - https://vk.com/misterlil"
                 send_message(chat_id, text=result)
+                try:
+                    if "ПОЯСНЕНИЕ" in result and "BlockInfinityErrore" not in result:
+                        message=message+"\n"
+                        s=message.split("\n") 
+                        line_sec=cms(message)[1]
+                        line_sec=int(line_sec.split(" ")[-1])
+                        for i in range(len(s)):
+                            if i == line_sec:
+                                s[i-1] = s[i-1] + " \u26A0"
+                        send_message(chat_id, text=str("\n".join(s)))
+                except BaseException:
+                    pass
         elif message == '/start':
-            send_message(chat_id, text="Инструкция:\n 1) Сначала введите ключ активации(его можно запросить у Lil Dojd - https://vk.com/misterlil).\n 2) Затем зарегистрируйтесь с паролем.\n\n-Если вы отправляете боту текст(код python), он его выполняет и выводит результат. В случа ошибки бот подскажет по какой причине она возникла.\n-Если вы отправляете боту фото, он распознает текст(код python) и отправлит его в сообщении. После этого вы можете внести поправки и отправить код(программу) боту для выполения.")
+            send_message(chat_id, text="Инструкция:\n 1) Сначала введите ключ активации. Его можно запросить у Lil Dojd - https://vk.com/misterlil.\n 2) Затем зарегистрируйтесь с паролем. Он необходим в случае переноса бота на другой сервис или при глобальном обновлении.")
         elif message == key() and check_key(user)=='no':
             add_key(user)
             send_message(chat_id, text="Ключ активирован!")
-            send_message(chat_id, text="Пожалуйста, зарегистрируйте пароль!\nНе менее 6 символов в длину, с латинскими буквами и цифрами.")
+            send_message(chat_id, text="Пожалуйста, зарегистрируйте пароль!\nНе менее 6 символов. Латинские буквы и цифры.")
         elif message != '' and len(message)>6 and str(message).isalpha()!=1 and str(message).isdigit()!=1 and check_key(user)=='yes' and check_pass(user)=='no' and m.match(str(message)):
             add_pass(user,message)
-            send_message(chat_id, text="Пароль зарегистрирован!")
+            send_message(chat_id, text="Аккаунт зарегистрирован!")
             send_message(chat_id, text="Можно приступать к работе с ботом!")
-            uspa=str(user+' | password:'+message)
-            send_message(676318616, text=uspa)
         elif message != '' and check_key(user)=='no':
             send_message(chat_id, text="Пожалуйста, введите ключ активации!")
         elif message != '' and check_key(user)=='yes' and check_pass(user)=='no':
-            send_message(chat_id, text="Пожалуйста, зарегистрируйте пароль!\nНе менее 6 символов в длину, с латинскими буквами и цифрами.")
+            send_message(chat_id, text="Пожалуйста, зарегистрируйте пароль!\nНе менее 6 символов. Латинские буквы и цифры.")
         return jsonify(r)
-    return '<h1>PM19.1 bot working now!</h1>'
+    return '<h1>Bot.py working now!</h1>'
 #-Flask
-
 
 if __name__ == '__main__':
     app.run()
+ 
